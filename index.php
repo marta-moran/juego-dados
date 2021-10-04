19b543396f1c38bee76291ddefd8415813669b4d
marta <martamoranl06@gmail.com>
2021-10-04 23:08:39 +0200

    código ordenado

--- index.php
+++ index.php
@@ -1,43 +1,38 @@
 <?php
-
-function tirarDados($jugador1, $dado)
-{
-                switch ($dado) {
-                        case 1:
-                                echo "&#9856;";
-                                break;
-                        case 2:
-                                echo  "&#9857;";
-                                break;
-                        case 3:
-                                echo "&#9858;";
-                                break;
-                        case 4:
-                                echo "&#9859;";
-                                break;
-                        case 5:
-                                echo "&#9860;";
-                                break;
-                        case 6:
-                                echo "&#9861;";
-                                break;
-                }
+function tirarDados($jugador1, $dado) {
+        switch ($dado) {
+                case 1:
+                        echo "&#9856;";
+                        break;
+                case 2:
+                        echo  "&#9857;";
+                        break;
+                case 3:
+                        echo "&#9858;";
+                        break;
+                case 4:
+                        echo "&#9859;";
+                        break;
+                case 5:
+                        echo "&#9860;";
+                        break;
+                case 6:
+                        echo "&#9861;";
+                        break;
         }
+}
 
-        function elGanador($jugador1, $jugador2) {
-                if (array_sum($jugador1) == array_sum($jugador2)) {
-                        echo "¡Empate!";
-                } else if (array_sum($jugador1) > array_sum($jugador2)) {
-                        echo "¡Gana el jugador 1";
-                } else {
-                        echo "¡Gana el jugador 2!";
-                }
+function elGanador($jugador1, $jugador2) {
+        if (array_sum($jugador1) == array_sum($jugador2)) {
+                echo "¡Empate!";
+        } else if (array_sum($jugador1) > array_sum($jugador2)) {
+                echo "¡Gana el jugador 1";
+        } else {
+                echo "¡Gana el jugador 2!";
         }
-
-
+}
 ?>
 <html>
-
 <head>
         <meta charset="UTF-8">
         <style>
@@ -45,16 +40,14 @@ function tirarDados($jugador1, $dado)
                         font-size: 30px;
                 }
 
-        
                 #dados {
                         font-size: 110px;
                 }
 
-
-
                 #resultado {
                         font-size: 20px;
                 }
+
         </style>
 </head>
 <body>
@@ -63,34 +56,34 @@ function tirarDados($jugador1, $dado)
                 <?php
 
                 echo "<p>Dado jugador 1</p>";
-                 
+
                 $entrada = array(0, 1, 2, 3, 4, 5, 6);
                 $dado = 0;
-                $jugador1 = [0, 0, 0, 0, 0,0];
+                $jugador1 = [0, 0, 0, 0, 0, 0];
                 for ($i = 0; $i < 6; $i++) {
                         $salidaDado = mt_rand(1, 6);
                         $dado = $entrada[$salidaDado];
                         "<span>" . tirarDados($jugador1, $dado) . "</span>";
-                        $jugador1[$i]=$dado;
+                        $jugador1[$i] = $dado;
                 }
-       
-        echo "<p>Dado jugador 2</p>";
-      
+
+                echo "<p>Dado jugador 2</p>";
+
                 for ($i = 0; $i < 6; $i++) {
                         $salidaDado = mt_rand(1, 6);
                         $dado = $entrada[$salidaDado];
                         "<span>" . tirarDados($jugador2, $dado) . "</span>";
-                        $jugador2[$i]=$dado;
+                        $jugador2[$i] = $dado;
                 }
                 ?>
         </div>
-        <div id = "resultado">
+        <div id="resultado">
                 <?php
-                echo "<br> suma jugador 1: " . array_sum($jugador1) . "<br>" ;
+                echo "<br> suma jugador 1: " . array_sum($jugador1) . "<br>";
                 echo "suma jugador 2: " . array_sum($jugador2) . "<br><br>";
                 elGanador($jugador1, $jugador2);
 
-                ?> 
+                ?>
         </div>
 </body>
 
